121074cddd1cdecdbaab85d21ecf61958d3cea64
tkhanh2003 <khanhlvt21.cdsg@saigontech.edu.vn>
2023-12-23 12:11:21 +0700

    push

---
+++ ShopbanquanaoSln/Shopbanquanao.Web.API/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shopbanquanao.Web.API.Models;
+
+public partial class Product
+{
+    public string? Name { get; set; }
+
+    public string? Price { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public string? Image { get; set; }
+
+    public int Id { get; set; }
+
+    public virtual Category? Category { get; set; }
+}
---
+++ ShopbanquanaoSln/Shopbanquanao.Web.API/Models/QlbhContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shopbanquanao.Web.API.Models;
+
+public partial class QlbhContext : DbContext
+{
+    public QlbhContext()
+    {
+    }
+
+    public QlbhContext(DbContextOptions<QlbhContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=HIHI\\SQLEXPRESS01;Database=qlbh;Trusted_Connection=true;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.ToTable("category");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable("product");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.CategoryId).HasColumnName("category_id");
+            entity.Property(e => e.Image)
+                .HasColumnType("text")
+                .HasColumnName("image");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .HasColumnName("name");
+            entity.Property(e => e.Price)
+                .HasMaxLength(50)
+                .HasColumnName("price");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK_product_category");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
